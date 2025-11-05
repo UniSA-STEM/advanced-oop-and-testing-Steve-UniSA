75f7e6918cf2ee52aea7b35106ba1b947bc567a7
Steve Corns <stevecorns@outlook.com.au>
2025-11-05 19:31:57 +1030

    Consolidated animal subclasses into animal.py per Matthew's advice. Added getters and setters to animal.py

--- animal.py
+++ animal.py
@@ -24,8 +24,37 @@ class Animal:
     def sleep(self):
         pass
 
+    def get_name(self):
+        return self._name
+
+    def get_species(self):
+        return self._species
+
+    def get_age(self):
+        return self._age
+
+    def get_dietary_requirements(self):
+        return self._dietary_requirements
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_species(self, species):
+        self._species = species
+
+    def set_age(self, age):
+        self._age = age
+
+    def set_dietary_requirements(self, dietary_requirements):
+        self._dietary_requirements = dietary_requirements
+
     def __str__(self):
         print(f"Name: {self._name}")
+
+    name = property(get_name, set_name)
+    species = property(get_species, set_species)
+    age = property(get_age, set_age)
+    dietary_requirements = property(get_dietary_requirements, set_dietary_requirements)
 
 
 class Bird(Animal):
