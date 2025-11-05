9eceef56e76a0e77e43ff2ea498321462bb8e57b
Steve Corns <stevecorns@outlook.com.au>
2025-11-05 19:47:49 +1030

    Added getters and setters to staff.py Added properties to staff.py Updated __str__

--- staff.py
+++ staff.py
@@ -9,11 +9,31 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 
 class Staff:
-    def __init__(self, name, role, environmental_type, cleanliness_level):
+    def __init__(self, name, role):
         self.__name = name
         self.__role = role
-        self.__environmental_type = environmental_type
-        self.__cleanliness_level = cleanliness_level
+        self.__duties = []
+
+    def __get_name(self):
+        return self.__name
+
+    def __get_role(self):
+        return self.__role
+
+    def __get_duties(self):
+        return self.__duties
+
+    def __set_name(self, name):
+        self.__name = name
+
+    def __set_role(self, role):
+        self.__role = role
+
+    def add_duties(self, duties):
+        self.__duties.append(duties)
+
+    def remove_duties(self, duties):
+        self.__duties.remove(duties)
 
     def feed_animal(self):
         pass
@@ -23,3 +43,10 @@ class Staff:
 
     def conduct_health_check(self):
         pass
+
+    def __str__(self):
+        return f"Name: {self.__name}\nRole: {self.__role}\nDuties: {self.__duties}"
+
+    name = property(__get_name, __set_name)
+    role = property(__get_role, __set_role)
+    duties = property(__get_duties)
