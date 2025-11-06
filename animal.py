5cee10bd77c003b5376fe5546e944b782fd6905c
Steve Corns <stevecorns@outlook.com.au>
2025-11-06 13:39:45 +1030

    Created abstract method for make_sound and added methods to the subclasses

--- animal.py
+++ animal.py
@@ -6,9 +6,10 @@ ID: 110457922
 Username: CORSY034
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from abc import ABC, abstractmethod
 
 
-class Animal:
+class Animal(ABC):
     def __init__(self, name, species, age, dietary_requirements):
         self._name = name
         self._species = species
@@ -28,25 +29,29 @@ class Animal:
         return self._dietary_requirements
 
     def set_name(self, name):
-        self._name = name
+        if isinstance(name, str):
+            self._name = name
 
     def set_species(self, species):
-        self._species = species
+        if isinstance(species, str):
+            self._species = species
 
     def set_age(self, age):
-        self._age = age
+        if isinstance(age, int) & age > 0 and age < 400:
+            self._age = age
 
     def set_dietary_requirements(self, dietary_requirements):
         self._dietary_requirements = dietary_requirements
 
+    @abstractmethod
     def make_sound(self):
-        pass
+        ...
 
     def eat(self):
-        pass
+        return f"{self._name} eats {self._dietary_requirements}."
 
     def sleep(self):
-        pass
+        return f"{self._name} is sleeping."
 
     def __str__(self):
         return f"Name: {self._name} Species: {self._species}"
@@ -61,12 +66,21 @@ class Bird(Animal):
     def __init__(self, name, species, age, dietary_requirements):
         super().__init__(name, species, age, dietary_requirements)
 
+    def make_sound(self):
+        return f"{self._name} chirps or squawks."
+
 
 class Mammal(Animal):
     def __init__(self, name, species, age, dietary_requirements):
         super().__init__(name, species, age, dietary_requirements)
 
+    def make_sound(self):
+        return f"{self._name} roars or growls."
+
 
 class Reptile(Animal):
     def __init__(self, name, species, age, dietary_requirements):
         super().__init__(name, species, age, dietary_requirements)
+
+    def make_sound(self):
+        return f"{self._name} hisses."
